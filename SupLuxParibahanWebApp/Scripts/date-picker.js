a0c2fb23fbab50ee341e3ae395556ef7295c8842
mjaumi <mjaumi2864@gmail.com>
2022-02-07 03:00:37 +0600

    date picker added

---
+++ SupLuxParibahanWebApp/Scripts/date-picker.js
@@ -0,0 +1,13 @@
+﻿$("#suplux-datepicker").datepicker({
+    minDate: 0,
+    maxDate: new Date(new Date().setMonth(new Date().getMonth() + 2)),
+    showOtherMonths: true,
+    changeMonth: true,
+    changeYear: true,
+    showAnim: "slideDown",
+    dateFormat: 'dd-M-yy'
+});
+
+$(".date").mousedown(function () {
+    $(".ui-datepicker").addClass("active");
+});
