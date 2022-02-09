1c74d08f96803a51dd52dbb9f0a033e787fed456
mjaumi <mjaumi2864@gmail.com>
2022-02-09 23:24:42 +0600

    search box fixed

--- SupLuxParibahanWebApp/Scripts/date-picker.js
+++ SupLuxParibahanWebApp/Scripts/date-picker.js
@@ -1,9 +1,12 @@
-﻿/*
+﻿const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const datePicker = document.getElementById('suplux-datepicker');
+const showDay = document.getElementById('suplux-day');
+
+/*
  *  picking the journey date
  */
-
-
-console.log('hi');
 $("#suplux-datepicker").datepicker({
     minDate: 0,
     maxDate: new Date(new Date().setMonth(new Date().getMonth() + 2)),
@@ -18,11 +21,12 @@ $("#suplux-datepicker").datepicker({
 
 $(".date").mousedown(function () {
 
-    $(".ui-datepicker").addClass("active")
-   
-   
+    $(".ui-datepicker").addClass("active");
 });
 
+/*
+ *  adding title to the calender
+ */
 setInterval(function () {
     if ($('#ui-datepicker-div').find('h5').length != 1) {
         $("#ui-datepicker-div").prepend('<h5 class="suplux-datepicker-title">Select journey date</h5>');
@@ -32,23 +36,32 @@ setInterval(function () {
 /*
  *  formatting the date and the day in the journey date picker 
  */
-const datePicker = document.getElementById('suplux-datepicker');
 
 datePicker.onchange = function () {
 
-    const showDay = document.getElementById('suplux-day');
-
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
     const fullDate = datePicker.value;
     const day = new Date(fullDate).getDay();
-
-    
 
     const date = fullDate.split(/\//);
     datePicker.value = date[1] + ' ' + months[--date[0]] + "'" + date[2].slice(2, 4);
 
     showDay.innerText = days[day];
-    
+}
+
+/*
+ *  setting current date when the home page loads
+ */
+window.onload = function () {
+
+    var d = new Date();
+
+    var month = d.getMonth() + 1;
+
+    var fullDate = (month < 10 ? '0' : '') + month + '/' + (d.getDate() < 10 ? '0' : '') + d.getDate() + '/' + d.getFullYear();
+    const day = new Date(fullDate).getDay();
+
+    const date = fullDate.split(/\//);
+    datePicker.value = date[1] + ' ' + months[--date[0]] + "'" + date[2].slice(2, 4);
+
+    showDay.innerText = days[day];
 }
