9b0d9e7b73359c3fb3852d5650dceee7803f53aa
mjaumi <mjaumi2864@gmail.com>
2022-03-11 05:25:02 +0600

    bus list page work on progress

---
+++ SupLuxParibahanWebApp/Controllers/BusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupLuxParibahanWebApp.Controllers
+{
+    public class BusController : Controller
+    {
+        // GET: Bus
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ SupLuxParibahanWebApp/Scripts/list.js
@@ -0,0 +1,14 @@
+﻿//toggling the search section
+document.getElementById('cross-btn').addEventListener('click', () => {
+
+    document.getElementById('cross-btn').classList.add('d-none');
+    document.getElementById('modify-search-container').classList.add('d-none');
+    document.getElementById('mobile-edit-hide-container').classList.remove('d-none');
+});
+
+document.getElementById('mobile-edit-btn').addEventListener('click', () => {
+
+    document.getElementById('cross-btn').classList.remove('d-none');
+    document.getElementById('modify-search-container').classList.remove('d-none');
+    document.getElementById('mobile-edit-hide-container').classList.add('d-none');
+});
