26e6b16633ac16567d8dbd2e723b46224a0dcb66
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2022-03-14 05:17:34 +0600

    transaction in userProfile done

---
+++ SupLuxParibahanWebApp/Models/DateFetchForTransaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SupLuxParibahanWebApp.Models
+{
+    public class DateFetchForTransaction
+    {
+        public string reservationDate { get; set; }
+        public string journeyDate { get; set; }
+    }
+}
